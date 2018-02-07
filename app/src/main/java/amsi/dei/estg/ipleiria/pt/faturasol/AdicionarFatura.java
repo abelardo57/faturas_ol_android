595a1fc3debfd2d00320d7d87d8b2d03175f8d0b
LHCCM <2160435@my.ipleiria.pt>
2018-02-07 00:20:25 +0000

    adicionar fatura confirmações

--- app/src/main/java/amsi/dei/estg/ipleiria/pt/faturasol/AdicionarFatura.java
+++ app/src/main/java/amsi/dei/estg/ipleiria/pt/faturasol/AdicionarFatura.java
@@ -2,6 +2,7 @@ package amsi.dei.estg.ipleiria.pt.faturasol;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -44,11 +45,12 @@ public class AdicionarFatura extends AppCompatActivity {
 
     public void onClickAdicionarCustomFatura(View view) {
 
-        if (txtNFatura.getText().toString().equals("")||txtNIFEmpresa.getText().toString().equals("")||txtNomeEmpresa.getText().toString().equals("")||txtMoradaEmpresa.getText().toString().equals("")||txtData.getText().toString().equals(""))
+        /*if (txtNFatura.getText().toString().equals("")||txtNIFEmpresa.getText().toString().equals("")||txtNomeEmpresa.getText().toString().equals("")||txtMoradaEmpresa.getText().toString().equals("")||txtData.getText().toString().equals(""))
         {
             Toast.makeText(this, "Campos vazios", Toast.LENGTH_SHORT).show();
         }
-        else {
+        else {*/
+        if (tentaRegisto() == true){
             //nova fatura
             try {
                 convertedDate = formatdate.parse(txtData.getText().toString());
@@ -65,4 +67,73 @@ public class AdicionarFatura extends AppCompatActivity {
         }
     }
 
+    private boolean tentaRegisto() {
+
+        // Reset errors.
+        txtNFatura.setError(null);
+        txtNIFEmpresa.setError(null);
+        txtNomeEmpresa.setError(null);
+        txtMoradaEmpresa.setError(null);
+        txtData.setError(null);
+
+
+        String numero = txtNFatura.getText().toString();
+        String nif = txtNIFEmpresa.getText().toString();
+        String nome = txtNomeEmpresa.getText().toString();
+        String morada = txtMoradaEmpresa.getText().toString();
+        String data = txtData.getText().toString();
+
+
+        boolean cancel = false;
+        View focusView = null;
+
+        /** Check numero not blank*/
+        if (numero.trim().isEmpty()) {
+            txtNFatura.setError(getString(R.string.error_field_required));
+            focusView = txtNFatura;
+            cancel = true;
+        }
+
+        /** Check for a valid nif.*/
+        if (TextUtils.isEmpty(nif)) {
+            txtNIFEmpresa.setError(getString(R.string.error_field_required));
+            focusView = txtNIFEmpresa;
+            cancel = true;
+        }
+        /** Check for valid nif length*/
+        else if (nif.length() != 9){
+            txtNIFEmpresa.setError(getString(R.string.error_invalid_nif));
+            focusView = txtNIFEmpresa;
+            cancel = true;
+        }
+
+        /** Check for a valid nome.*/
+        if (TextUtils.isEmpty(nome)) {
+            txtNomeEmpresa.setError(getString(R.string.error_field_required));
+            focusView = txtNomeEmpresa;
+            cancel = true;
+        }
+
+        /** Check for a valid morada.*/
+        if (TextUtils.isEmpty(morada)) {
+            txtMoradaEmpresa.setError(getString(R.string.error_field_required));
+            focusView = txtMoradaEmpresa;
+            cancel = true;
+        }
+
+        /** Check for a valid data.*/
+        if (TextUtils.isEmpty(data)) {
+            txtData.setError(getString(R.string.error_field_required));
+            focusView = txtData;
+            cancel = true;
+        }
+
+        if (cancel == true) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+            return false;
+        }
+        return true;
+    }
 }
